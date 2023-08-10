b565e78d48415117c638a9785763b029a2db5799
RohitM-IN <rohit.mahajan.in@gmail.com>
2023-08-10 22:16:38 +0530

    Add project files.

---
+++ DBLayer/Connection.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+
+namespace DBLayer
+{
+    public class Connection
+    {
+        private string _connectionString;
+
+        public Connection()
+        {
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string folderPath = Path.Combine(documentsPath, "YourDatabaseFolder");
+            Directory.CreateDirectory(folderPath);
+
+            string databasePath = Path.Combine(folderPath, "YourDatabaseName.db");
+            _connectionString = $"Data Source={databasePath};Version=3;";
+        }
+
+        public SQLiteConnection OpenConnection()
+        {
+            SQLiteConnection connection = new SQLiteConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        public void CloseConnection(SQLiteConnection connection)
+        {
+            connection.Close();
+        }
+
+        public void InitDatabase()
+        {
+            using (SQLiteConnection connection = OpenConnection())
+            {
+                // Create database tables if they don't exist
+                string createUserTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS Users (
+                        Id INTEGER PRIMARY KEY,
+                        Username TEXT NOT NULL,
+                        Email TEXT NOT NULL
+                    );";
+
+                using (SQLiteCommand command = new SQLiteCommand(createUserTableQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                // Add more table creation queries here if needed
+            }
+        }
+
+        public List<User> GetAllUsers()
+        {
+            List<User> users = new List<User>();
+
+            using (SQLiteConnection connection = OpenConnection())
+            {
+                string selectQuery = "SELECT Id, Username, Email FROM Users;";
+                using (SQLiteCommand command = new SQLiteCommand(selectQuery, connection))
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        string username = reader.GetString(1);
+                        string email = reader.GetString(2);
+
+                        users.Add(new User { Id = id, Username = username, Email = email });
+                    }
+                }
+            }
+
+            return users;
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+            public string Username { get; set; }
+            public string Email { get; set; }
+        }
+    }
+}
---
+++ SqliteTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using DBLayer;
+using System.Data.SQLite;
+using static DBLayer.Connection;
+
+namespace SqliteTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Connection connection = new Connection();
+
+            SQLiteConnection dbConnection = connection.OpenConnection();
+            connection.InitDatabase();
+
+
+            // Example: Insert data into the Users table
+            string insertQuery = "INSERT INTO Users (Username, Email) VALUES (@Username, @Email);";
+            using (SQLiteCommand command = new SQLiteCommand(insertQuery, dbConnection))
+            {
+                command.Parameters.AddWithValue("@Username", "exampleUser");
+                command.Parameters.AddWithValue("@Email", "user@example.com");
+                command.ExecuteNonQuery();
+            }
+
+            // Close the connection when done
+
+            List<User> users = connection.GetAllUsers();
+            foreach (User user in users)
+            {
+                Console.WriteLine($"ID: {user.Id}, Username: {user.Username}, Email: {user.Email}");
+            }
+
+
+            connection.CloseConnection(dbConnection);
+
+            Console.ReadLine();
+        }
+    }
+}
